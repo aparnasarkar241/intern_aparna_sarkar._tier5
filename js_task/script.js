680a5213fe951e088eb219114b9833b50880a478
aparna241 <aparnasarkar241@gmail.com>
2021-07-23 21:10:15 +0530

    js task

---
+++ js_task/script.js
@@ -0,0 +1,30 @@
+var array=[];
+var input= document.forms["form"]["fName"];
+var studentList= document.getElementById("stdList");
+// console.log(input);
+
+function getData(){
+
+    if(input.value===""){
+        alert("Name must filled out")
+    }else{
+        array.push(input.value)
+        // console.log(array);
+        show();
+    }
+    function show(){
+        studentList.innerHTML="";
+        array.forEach(function(item, index){
+            studentList.innerHTML+= `<li> ${item}<span><button class="button2" type="submit" onclick='deleteItem("${index}")'>remove</button></span></li>`;
+
+            
+        })
+    }
+    function deleteItem() {
+        array.splice(index, 1)
+        show()
+    }
+    
+
+
+}
